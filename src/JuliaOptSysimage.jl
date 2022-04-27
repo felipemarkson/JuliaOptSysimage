816af935ae27be0a8814b918a1b7ab4509fbdcf9
Felipe Markson dos Santos Monteiro <47871564+felipemarkson@users.noreply.github.com>
2022-04-27 16:37:03 -0300

    Fixing path issue

--- src/JuliaOptSysimage.jl
+++ src/JuliaOptSysimage.jl
@@ -5,8 +5,8 @@ function create()
 
     create_sysimage(
         ["Revise", "JuMP", "Plots", "DataFrames", "CSV"];
-        sysimage_path="sysimage/sysimage.so",
-        precompile_execution_file="src/imports.jl"
+        sysimage_path="JuliaOptSysimage/sysimage/sysimage.so",
+        precompile_execution_file="JuliaOptSysimage/src/imports.jl"
     )
 
 
